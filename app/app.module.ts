c062187b3b0b461637440afebe46373051025b70
Sudhi Pulla <sudhi.pulla@solarix.co.nz>
2017-02-02 15:31:43 +1300

    Completed “Step 6: Routing”

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroDetailComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroDetailComponent
+  ],
+
+  providers: [
+    HeroService,
+    { provide: APP_BASE_HREF, useValue: '' }
+  ],
+
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
